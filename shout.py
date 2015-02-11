c0a3bf79de31d89238d3fabbe63d09654e85a919
Battleroid <casweed@gmail.com>
2015-02-11 00:12:35 -0500

    split up template into multiple smaller templates, removed AJAX whatnot (for now at least), begun work for storing votes in database instead of just client based sessions

--- shout.py
+++ shout.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from flask import Flask, url_for, redirect, render_template, session, flash
+from flask import Flask, url_for, redirect, render_template, session, flash, jsonify
 from sqlalchemy import exc
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form, RecaptchaField
@@ -37,6 +37,25 @@ class Shout(db.Model):
     def __repr__(self):
         return '<Shout %r at %r>' % (self.shout, self.created)
 
+class Vote(db.Model):
+    __tablename__ = 'vote'
+
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(15), nullable=False)
+    vote_time = db.Column(db.DateTime, default=datetime.now())
+    can_vote= db.Column(db.DateTime, default=timedelta(days=1) + datetime.now())
+
+    def __init__(self, ip):
+        self.ip = ip
+
+    def able_to_vote(self):
+        return datetime.now() > self.can_vote
+
+    def can_vote_when(self):
+        return humanize.naturaltime(self.can_vote)
+
+    def __repr__(self):
+        return '<Vote %s at %s, can vote in %s>' % (self.ip, self.vote_time, self.can_vote_when())
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -45,9 +64,10 @@ def add():
         try:
             db.session.add(Shout(form.shout_text.data[:140]))
             db.session.commit()
+            flash('Yay! Your post has been submitted!')
         except exc.SQLAlchemyError:
             db.session.rollback()
-        flash('Shout added.')
+            flash('Problem submitting post!', 'error')
     else:
         flash_errors(form)
     return redirect(url_for('index'))
@@ -56,7 +76,7 @@ def add():
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
-            flash('Error in %s: %s' % (getattr(form, field).label.text, error))
+            flash('Error in %s: %s' % (getattr(form, field).label.text, error), 'error')
 
 
 @app.route('/like/<int:shout_id>', methods=['GET'])
@@ -71,7 +91,7 @@ def like(shout_id):
     else:
         vote_time = session[shout_id] + timedelta(days=1)
         if vote_time > datetime.now():
-            flash('You need to wait %s to vote on that again.' % humanize.naturaltime(vote_time))
+            flash('You need to wait %s to vote on that again.' % humanize.naturaltime(vote_time), 'error')
         else:
             set_vote_session(shout_id)
             target.like()
@@ -92,7 +112,7 @@ def dislike(shout_id):
     else:
         vote_time = session[shout_id] + timedelta(days=1)
         if vote_time > datetime.now():
-            flash('You need to wait %s to vote on that again.' % humanize.naturaltime(vote_time))
+            flash('You need to wait %s to vote on that again.' % humanize.naturaltime(vote_time), 'error')
         else:
             set_vote_session(shout_id)
             target.dislike()
@@ -115,7 +135,7 @@ class AddForm(Form):
 def index():
     form = AddForm()
     shouts = Shout.query.order_by(Shout.likes.desc()).limit(10).all()
-    return render_template('index.html', title='Index', form=form, shouts=shouts)
+    return render_template('index.html', title='Shout', form=form, shouts=shouts)
 
 
 @app.template_filter()
